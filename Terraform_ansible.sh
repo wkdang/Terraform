a170b45fe625a6f6f5c2cc9be5ac987133fc872d
wkdang <36007806+wkdang@users.noreply.github.com>
2020-05-13 06:54:04 +0200

    Creation

---
+++ Terraform_ansible.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#set -e
+
+Var_SubscriptionId='5c867415-9d26-445f-9a75-9bc80347d771'
+Var_github='https://github.com/wkdang/sap-hana.git'
+Var_SubscriptionId=$1
+Var_github=$2
+
+echo "Début installation Prérequis" >> /tmp/parameter.txt
+#Installation du gestionnaire de paquets PIP (python-pip)
+sudo apt-get update && sudo apt-get install -y libssl-dev libffi-dev python-dev python-pip
+
+#Installation de unzip
+sudo apt-get install unzip
+echo "Fin installation Prérequis" >> /tmp/parameter.txt
+
+#Installation AzureCLI
+echo "Début installation AzureCLI" >> /tmp/parameter.txt
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+echo "Fin installation AzureCLI" >> /tmp/parameter.txt
+
+#Installation Terraform (https://www.terraform.io/downloads.html)
+echo "Début installation Terraform" >> /tmp/parameter.txt
+wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
+unzip terraform_0.12.24_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+rm terraform_0.12.24_linux_amd64.zip
+
+#Check installation Terraform
+echo "Version Terraform installée :" >> /tmp/parameter.txt
+terraform --version >> /tmp/parameter.txt
+echo "Fin installation Terraform" >> /tmp/parameter.txt
+
+#Installation des packages Ansible avec les modules Azure
+echo "Début installation Ansible" >> /tmp/parameter.txt
+sudo pip install ansible[azure]
+
+#Check installation Ansible
+echo "Version Ansible installée :" >> /tmp/parameter.txt
+ansible --version >> /tmp/parameter.txt
+echo "Fin installation Ansible" >> /tmp/parameter.txt
+
+echo "Début installation SPN" >> /tmp/parameter.txt
+# Créer un service principal pour manager les ressources Azures
+var_spn=$(az ad sp create-for-rbac --name saphana_spn)
+var_sp_appid=${var_spn:14:36}
+var_sp_secret=${var_spn:132:36}
+var_sp_tenantid=${var_spn:184:36}
+#Création des variables d'environnement
+echo "# configure service principal for Ansible" >> /tmp/set-sp.sh
+echo "export AZURE_SUBSCRIPTION_ID='"$Var_SubscriptionId"'" >> /tmp/set-sp.sh
+echo "export AZURE_CLIENT_ID='"$var_sp_appid"'" >> /tmp/set-sp.sh
+echo "export AZURE_SECRET='"$var_sp_secret"'" >> /tmp/set-sp.sh
+echo "export AZURE_TENANT='"$var_sp_tenantid"'" >> /tmp/set-sp.sh
+echo "# configure service principal for Terraform" >> /tmp/set-sp.sh
+echo "export ARM_SUBSCRIPTION_ID='"$Var_SubscriptionId"'" >> /tmp/set-sp.sh
+echo "export ARM_CLIENT_ID='"$var_sp_appid"'" >> /tmp/set-sp.sh
+echo "export ARM_CLIENT_SECRET='"$var_sp_secret"'" >> /tmp/set-sp.sh
+echo "export ARM_TENANT_ID='"$var_sp_tenantid"'" >> /tmp/set-sp.sh
+# Application des commandes d'export
+source /tmp/set-sp.sh
+echo "Fin installation SPN" >> /tmp/parameter.txt
+
+# Création de la clé SSH
+echo "Début installation clé SSH" >> /tmp/parameter.txt
+ssh-keygen -o -f ~/.ssh/id_rsa -N '' -y
+echo "Fin installation clé SSH" >> /tmp/parameter.txt
+
+echo "Début Copie Github" >> /tmp/parameter.txt
+# Copie du Github
+git clone https://github.com/wkdang/sap-hana.git
+echo "Fin Copie Github" >> /tmp/parameter.txt
---
+++ main.tf
@@ -0,0 +1,90 @@
+variable "prefix" {
+  default = "terraform"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "Francecentral"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+   delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+   delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+  os_profile {
+    computer_name  = ${var.prefix}
+    admin_username = "Terraform"
+    admin_password = "Welcome123456"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "Terraform"
+  }
+resource "azurerm_virtual_machine_extension" "main" {
+  name                 = ${var.prefix}
+  virtual_machine_id   = azurerm_virtual_machine.main.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+    {
+        "fileUris": [ "https://raw.githubusercontent.com/wkdang/LVM-SAPHANA-NAKISA/master/scripts/Terraform_ansible.sh" ],
+        "commandToExecute": "[concat('sh Terraform_ansible.sh ', parameters('customUri'), ' ' , parameters('vmUserName'))]"
+    }
+SETTINGS
+
+
+  tags = {
+    environment = "Production"
+  }
+}
